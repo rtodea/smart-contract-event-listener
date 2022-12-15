0804743b611690236db9db4c5f162015ec65da6b
Robert Todea <rtodea@theladders.com>
2022-12-15 23:25:32 +0200

    Fix import order

--- listener/sample/kafka/consumer/index.js
+++ listener/sample/kafka/consumer/index.js
@@ -1,56 +1,55 @@
-const ip = require('ip')
+import ip from "ip";
+import { Kafka, logLevel } from "kafkajs";
 
-const { Kafka, logLevel } = require('kafkajs')
-
-const host = process.env.HOST_IP || ip.address()
+const host = process.env.HOST_IP || ip.address();
 
 const kafka = new Kafka({
   logLevel: logLevel.INFO,
   brokers: [`${host}:9092`],
-  clientId: 'example-consumer',
-})
+  clientId: "example-consumer"
+});
 
-const topic = 'topic-test'
-const consumer = kafka.consumer({ groupId: 'test-group' })
+const topic = "topic-test";
+const consumer = kafka.consumer({ groupId: "test-group" });
 
 const run = async () => {
-  await consumer.connect()
-  await consumer.subscribe({ topic, fromBeginning: true })
+  await consumer.connect();
+  await consumer.subscribe({ topic, fromBeginning: true });
   await consumer.run({
     // eachBatch: async ({ batch }) => {
     //   console.log(batch)
     // },
     eachMessage: async ({ topic, partition, message }) => {
-      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
-      console.log(`- ${prefix} ${message.key}#${message.value}`)
-    },
-  })
-}
+      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`;
+      console.log(`- ${prefix} ${message.key}#${message.value}`);
+    }
+  });
+};
 
-run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+run().catch(e => console.error(`[example/consumer] ${e.message}`, e));
 
-const errorTypes = ['unhandledRejection', 'uncaughtException']
-const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+const errorTypes = ["unhandledRejection", "uncaughtException"];
+const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2"];
 
 errorTypes.forEach(type => {
   process.on(type, async e => {
     try {
-      console.log(`process.on ${type}`)
-      console.error(e)
-      await consumer.disconnect()
-      process.exit(0)
+      console.log(`process.on ${type}`);
+      console.error(e);
+      await consumer.disconnect();
+      process.exit(0);
     } catch (_) {
-      process.exit(1)
+      process.exit(1);
     }
-  })
-})
+  });
+});
 
 signalTraps.forEach(type => {
   process.once(type, async () => {
     try {
-      await consumer.disconnect()
+      await consumer.disconnect();
     } finally {
-      process.kill(process.pid, type)
+      process.kill(process.pid, type);
     }
-  })
-})
+  });
+});
