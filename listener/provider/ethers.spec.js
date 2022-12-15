0b4d19c7f4e6c41ff5dbef1a016cd6d97b465038
Robert Todea <rtodea@theladders.com>
2022-12-15 16:53:22 +0200

    Listen to events with ethers.js

--- listener/provider/ethers.spec.js
+++ listener/provider/ethers.spec.js
@@ -1,22 +1,45 @@
 import ethers from "ethers";
 import dotenv from "dotenv";
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { readFileSync } from "fs";
 
 describe("ethers.js integration", () => {
   let provider;
   let contract;
   before(async () => {
     dotenv.config();
-    const {INFURA_WS_URL, INFURA_API_KEY} = process.env;
+    const { INFURA_WS_URL, INFURA_API_KEY } = process.env;
     const webSocketProviderUrl = [INFURA_WS_URL, INFURA_API_KEY].join("/");
     provider = new ethers.providers.WebSocketProvider(webSocketProviderUrl);
 
-    const {SAMPLE_CONTRACT_ADDRESS, SAMPLE_CONTRACT_ABI} = process.env;
-    // read JSON from SAMPLE_CONTRACT_ABI path
+    const { SAMPLE_CONTRACT_ADDRESS, SAMPLE_CONTRACT_ABI } = process.env;
+    // read JSON from SAMPLE_CONTRACT_ABI path with fs.readFileSync
+    const sampleContractABI = JSON.parse(readFileSync(SAMPLE_CONTRACT_ABI, { encoding: "utf8" }));
+
     // const abi
-    contract = new ethers.Contract()
+    contract = new ethers.Contract(SAMPLE_CONTRACT_ADDRESS, sampleContractABI, provider);
   });
 
-  it("should listen to contract events", () => {
+  it("should listen to contract events", (done) => {
+    contract.once("*", (...args) => {
+      expect(args).to.not.be.undefined;
+      console.log(args);
+      done();
+    });
+  }).timeout("10m");
 
-  })
+  it("should listen to eth events", (done) => {
+    const filter = {
+      address: process.env.SAMPLE_CONTRACT_ADDRESS,
+      topics: [
+        ethers.utils.id("Transfer(address,address,uint256)"),
+      ]
+    }
+    provider.once(filter, (log, event) => {
+      console.log(log, event);
+      expect(log).to.not.be.undefined;
+      done();
+    })
+  }).timeout("1m");
 });
